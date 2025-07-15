265d1e458a4213a0c796e8e260079555748f7c57
Lia <sethroeback1@gmail.com>
2025-07-15 10:50:09 -0600

    added more clicker stuff

--- FirstWebsite/clicker.js
+++ FirstWebsite/clicker.js
@@ -1,4 +1,4 @@
-let score =0;
+let score = 0;
 const scoreDisplay = document.getElementById("score");
 const clickButton = document.getElementById("click-button");
 clickButton.addEventListener("click",() =>{
@@ -6,3 +6,23 @@ clickButton.addEventListener("click",() =>{
     scoreDisplay.textContent = score;
 });
 const choices = ["rock","paper","scissors"];
+const playerChoiceDisplay = document.getElementById("player-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const resultDisplay = document.getElementById("rps-result");
+document.querySelectorAll(".rps").forEach(button => {
+    button.addEventListener("click", () => {
+        const playerChoice = button.dataset.choice;
+        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        playerChoiceDisplay.textContent = playerChoice;
+        computerChoiceDisplay.textContent = computerChoice;
+        resultDisplay.textContent = getResult(playerChoice, computerChoice);
+    });
+});
+function getResult(player,computer){
+    if(player == computer) return "Its a TIE!!";
+    else if(player === "rock" && computer === "scissors" ||
+            player === "scissors" && computer === "paper" ||
+            player === "paper" && computer === "rock") 
+        return "You WIN!!!";
+    else return "You Lose!!!";   
+}
