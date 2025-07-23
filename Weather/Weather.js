182d65d9772e86045ef7b8259f09bf8fde9d9d8b
Lia <sethroeback1@gmail.com>
2025-07-23 10:17:04 -0600

    added some stuff

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -1,7 +1,11 @@
 //d3df5d3c92ea812769bd9ad5b87c118f
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Erie,pennsylvania,US&appid=d3df5d3c92ea812769bd9ad5b87c118f&units=imperial')
+const searchButton = document.getElementById("citySearchButton");
+searchButton.addEventListener("click",() =>{
+    cityName = document.getElementById("city").value;
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=erie,pennsylvania,US&appid=d3df5d3c92ea812769bd9ad5b87c118f&units=imperial')
   .then(response => response.json())
   .then(data => {
-    document.getElementById()
+    //document.getElementById()
   })
   .catch(error => console.error('Error:', error));
+});
