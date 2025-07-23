e7693b93bf48cc677dff023d4a87702a5562fb16
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-07-23 11:33:46 -0600

    ya

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -1,11 +1,28 @@
 //d3df5d3c92ea812769bd9ad5b87c118f
 const searchButton = document.getElementById("citySearchButton");
+const oneDay = document.getElementById("citySearchButton");
+const fiveDay = document.getElementById("citySearchButton");
+
+oneDayForcast = true;
+fiveDayForcast = false;
+oneDay.addEventListener("click", ()=>{
+  oneDayForcast = true;
+  fiveDayForcast = false;
+});
+fiveDay.addEventListener("click", ()=>{
+  oneDayForcast = false;
+  fiveDayForcast = true;
+});
 searchButton.addEventListener("click",() =>{
     cityName = document.getElementById("city").value;
     stateName = document.getElementById("state").value;
     countryName = document.getElementById("country").value;
     apiKey = "d3df5d3c92ea812769bd9ad5b87c118f";
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
+    if(oneDayForcast){
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
+    }else{
+      url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
+    }
     fetch(url)
   .then(response => response.json())
   .then(data => {
