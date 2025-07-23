3900dd23ff31018d111c818b3e6ce78ea3494be5
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-07-23 10:43:10 -0600

    let you search citys

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -2,10 +2,17 @@
 const searchButton = document.getElementById("citySearchButton");
 searchButton.addEventListener("click",() =>{
     cityName = document.getElementById("city").value;
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=erie,pennsylvania,US&appid=d3df5d3c92ea812769bd9ad5b87c118f&units=imperial')
+    stateName = document.getElementById("state").value;
+    countryName = document.getElementById("country").value;
+    apiKey = "d3df5d3c92ea812769bd9ad5b87c118f";
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
+    fetch(url)
   .then(response => response.json())
   .then(data => {
     //document.getElementById()
+    console.log("Temperature:", data.main.temp);
+    console.log("Weather Condition:", data.weather[0].main);
+    console.log("Humidity:", data.main.humidity);
   })
   .catch(error => console.error('Error:', error));
 });
