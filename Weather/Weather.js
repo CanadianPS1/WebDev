3c7b62f095dda9220c3be5f13877f99de0b2d3a0
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-07-24 15:04:29 -0600

    fixed the among us

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -1,35 +1,44 @@
 //d3df5d3c92ea812769bd9ad5b87c118f
 const searchButton = document.getElementById("citySearchButton");
-const oneDay = document.getElementById("citySearchButton");
-const fiveDay = document.getElementById("citySearchButton");
+const oneDay = document.getElementById("oneDay");
+const fiveDay = document.getElementById("fiveDay");
 
-oneDayForcast = true;
-fiveDayForcast = false;
+let oneDayForcast = true;
+let fiveDayForcast = false;
 oneDay.addEventListener("click", ()=>{
-  oneDayForcast = true;
-  fiveDayForcast = false;
+    oneDayForcast = true;
+    fiveDayForcast = false;
 });
 fiveDay.addEventListener("click", ()=>{
-  oneDayForcast = false;
-  fiveDayForcast = true;
+    oneDayForcast = false;
+    fiveDayForcast = true;
 });
 searchButton.addEventListener("click",() =>{
-    cityName = document.getElementById("city").value;
-    stateName = document.getElementById("state").value;
-    countryName = document.getElementById("country").value;
+    const cityName = document.getElementById("city").value;
+    const stateName = document.getElementById("state").value;
+    const countryName = document.getElementById("country").value;
     apiKey = "d3df5d3c92ea812769bd9ad5b87c118f";
     if(oneDayForcast){
       url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
     }else{
       url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
     }
-    fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    //document.getElementById()
-    console.log("Temperature:", data.main.temp);
-    console.log("Weather Condition:", data.weather[0].main);
-    console.log("Humidity:", data.main.humidity);
-  })
-  .catch(error => console.error('Error:', error));
+    fetch(url).then(response => response.json()).then(data => {
+        console.clear();
+        console.log("~" + cityName + ", " + stateName + "~");
+        if(oneDayForcast){
+            console.log("Temperature:", data.main.temp);
+            console.log("Weather Condition:", data.weather[0].main);
+            console.log("Humidity:", data.main.humidity);
+        }else{
+            for(i = 0; i < 5; i++){
+                console.log("Day: " + (i + 1));
+                console.log("Temperature:", data.list[i].main.temp);
+                console.log("Weather Conditions: ", data.list[i].weather[0].main);
+                console.log("Humidity: ", data.list[0].main.humidity);
+            }
+            
+        }
+        
+    }).catch(error => console.error('Error:', error));
 });
