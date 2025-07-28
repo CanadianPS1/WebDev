7e7c26f47fbb336a35e634393b1c23b7238fd8c8
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-07-27 21:50:49 -0600

    tryed to add fiveDay

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -30,10 +30,10 @@ searchButton.addEventListener("click",() =>{
             document.getElementById("weatherWidget").src = "FiveDay.html";
             url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateName},${countryName}&appid=${apiKey}&units=imperial`;
         }
-        localStorage.setItem("city", cityName);
-        localStorage.setItem("state", stateName);
-        localStorage.setItem("country", countryName);
-        localStorage.setItem("forcast", oneDayForcast);
+        // localStorage.setItem("city", cityName);
+        // localStorage.setItem("state", stateName);
+        // localStorage.setItem("country", countryName);
+        // localStorage.setItem("forcast", oneDayForcast);
         fetch(url).then(response => response.json()).then(data => {
             console.clear();
             console.log("~" + cityName + ", " + stateName + "~");
@@ -56,12 +56,23 @@ searchButton.addEventListener("click",() =>{
                 iframe.src = "OneDay.html"
             }else{
                 for(i = 0; i < 5; i++){
-                    console.log("Day: " + (i + 1));
-                    console.log("Temperature:", data.list[i].main.temp);
-                    console.log("Weather Conditions: ", data.list[i].weather[0].main);
-                    console.log("Humidity: ", data.list[0].main.humidity);
-                }
-                
+                    const iframe = document.getElementById("weatherWidget");
+                    iframe.onload = () => {
+                    try{
+                        const iconCode = data.weather[0].icon;
+                        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+                        iframe.contentWindow.document.getElementById(`day${i}Image`).src = iconUrl;
+                        const tempEl = iframe.contentWindow.document.getElementById(`d${i}temp`);
+                        if(tempEl){
+                            tempEl.textContent = data.main.temp_min + "°F / " + data.main.temp_max + "°F";
+                            console.log("Updated iframe textContent");
+                        }else console.error("Element with id 'temp' not found in iframe.");
+                    }catch (e){
+                        console.error("Access to iframe DOM denied:", e);
+                    }
+                    };
+                    iframe.src = "FiveDay.html"
+                    }
             }
         }).catch(error => console.error('Error:', error));
     });
